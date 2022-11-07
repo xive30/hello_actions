9dbd9fda38742b8877d51bc13eb01cc527319d06
xive30 <deflo59@gmail.com>
2022-11-07 13:02:05 +0100

    new port

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ const app = express()
 
 app.get('/hello', (req, res) => res.send('Hello Simplon 1'));
 
-app.listen(3000, ()=> console.log("Application démarrée sur le port 3000"));
+app.listen(3002, ()=> console.log("Application démarrée sur le port 3002"));
