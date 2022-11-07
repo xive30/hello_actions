bb15a473135d08e9fe623682173744836ede989a
xive30 <deflo59@gmail.com>
2022-11-07 13:34:15 +0100

    add cy

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express()
 
-app.get('/hello', (req, res) => res.send('Hello Simplon 1'));
+app.get('/hello', (req, res) => res.send('Hello Florent'));
 
 app.listen(3002, ()=> console.log("Application démarrée sur le port 3002"));
