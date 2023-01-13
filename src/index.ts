ad2985b8b665962c4417dd78b6c669dcfcefd358
xive30 <deflo59@gmail.com>
2023-01-13 15:33:09 +0100

    test docker5

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
+
 
 const app = express()
 
-app.get('/hello', (req, res) => res.send('Hello Florent bon week end'));
+app.get('/hello', (req, res) => res.send('Hello Florent' +  process.env.id ));
 
 app.listen(3001, ()=> console.log("Application démarrée sur le port 3001"));
