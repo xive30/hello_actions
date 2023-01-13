111dcd9aff4631e02db6f26dccb672cb93c876f7
xive30 <deflo59@gmail.com>
2023-01-13 15:27:31 +0100

    test docker4

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express()
 
-app.get('/hello', (req, res) => res.send('Hello Florent'));
+app.get('/hello', (req, res) => res.send('Hello Florent bon week end'));
 
 app.listen(3001, ()=> console.log("Application démarrée sur le port 3001"));
